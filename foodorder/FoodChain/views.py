3e38b8d63bf2c01f9fc450b0fa8943c07d9ca2a0
DELTA <sreeragmsurendran199>
2019-02-01 01:42:47 -0800

    started

--- foodorder/FoodChain/views.py
+++ foodorder/FoodChain/views.py
@@ -4,15 +4,35 @@ from .models import Dishes,Place,Restorent
 # Create your views here.
 class DishLstView(ListView):
     model = Dishes
-    template_name =''
+    template_name ='dishesl.html'
     context_object_name = 'dishlist'
-    def
+    def get_queryset(self):
+        return Dishes.objects.all()
 
 class PlaceListView(ListView):
     model = Place
     template_name = ''
+    context_object_name = 'placelist'
+    def get_queryset(self):
+        return Place.objects.all()
 
 class RestListView(ListView):
     model = Restorent
     template_name = ''
+    context_object_name = 'restlist'
+    def get_queryset(self):
+        return Restorent.objects.all()
+class DishDetailedView(DetailView):
+    model = Dishes
+    template_name = ''
+    context_object_name = 'dishd'
 
+class PlaceDetailedView(DetailView):
+    model = Place
+    template_name = ''
+    context_object_name = 'placed'
+
+class RestDetailedView(DetailView):
+    model = Restorent
+    template_name = ''
+    context_object_name = 'restd'
