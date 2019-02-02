57f1250d8148a6d4fdb3eb6317a1c45367681136
DELTA <sreeragmsurendran199>
2019-02-02 13:25:35 +0530

    started

--- foodorder/FoodChain/urls.py
+++ foodorder/FoodChain/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('dishes/', DishLstView.as_view(), name='dishlist'),
     path('places/',PlaceListView.as_view(),name='places'),
     path('restaurent/',RestListView.as_view(),name='restorents'),
-    path('<int:pk>',DishDetailedView.as_view(),name='dishdetails'),
+    path('<int:pk>/',DishDetailedView.as_view(),name='dishdetails'),
     path('placedetails/',PlaceDetailedView.as_view(),name='placedetails'),
     path('restaurentdetails/',RestDetailedView.as_view(),name='restaurentDetails'),
 ]
